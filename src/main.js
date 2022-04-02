1c46a51218eff2021066132d37c54c498c970acc
rafalq <kruszewskirafael@gmail.com>
2022-04-02 15:39:34 +0100

    set up vue router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [],
+});
+
+createApp(App).use(router).mount('#app');
