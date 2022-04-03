047da7ccae22ae2f5622a5d6ac2e43bc92da82fe
rafalq <kruszewskirafael@gmail.com>
2022-04-03 17:48:23 +0100

    design ViewNotes page

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="notes">
-    <h1>Notes</h1>
-  </div>
+	<div class="notes">
+		<div class="card mb-4" v-for="i in 3">
+			<div class="card-content">
+				<div class="content">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+					iaculis mauris.
+					<br />
+					<time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+				</div>
+			</div>
+			<footer class="card-footer">
+				<a href="#" class="card-footer-item">Edit</a>
+				<a href="#" class="card-footer-item">Delete</a>
+			</footer>
+		</div>
+	</div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
