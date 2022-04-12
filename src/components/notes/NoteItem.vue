9196d44a78a6535ae51e4226261782197bd688be
rafalq <kruszewskirafael@gmail.com>
2022-04-12 15:15:58 +0100

    add computed property for a note length

---
+++ src/components/notes/NoteItem.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="card mb-4">
+		<div class="card-content">
+			<div class="content">
+				<div
+					class="has-text-right has-text-success mb-2"
+				>
+					<small>{{ charLength }}</small>
+				</div>
+				{{ note.content }}
+				<div class="mt-2 has-text-grey">
+					<small>
+						<time>
+							{{ note.createdAt }}
+						</time>
+					</small>
+				</div>
+			</div>
+		</div>
+		<footer class="card-footer">
+			<a href="#" class="card-footer-item"
+				>Edit</a
+			>
+			<a href="#" class="card-footer-item"
+				>Delete</a
+			>
+		</footer>
+	</div>
+</template>
+
+<script setup>
+/* 
+  imports
+*/
+
+import { computed } from "vue";
+
+/* 
+    props
+*/
+
+const props = defineProps({
+	note: {
+		type: Object,
+		required: true,
+	},
+});
+
+/* 
+    character length 
+*/
+
+const charLength = computed(() => {
+	let text = props.note.content;
+	let textNoSpaces = text.replace(/\s/g, "");
+	let length = textNoSpaces.length;
+	let description =
+		length === 1 ? "character" : "characters";
+	return `${length} ${description}`;
+});
+</script>
+
+<style scoped></style>
